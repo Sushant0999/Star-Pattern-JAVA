a75408a47ddbd27f039b555a092433e2f798f7b2
Sushant0999 <66193982+Sushant0999@users.noreply.github.com>
2021-07-31 12:18:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n = 5;
+        //Outer Loop for number of Rows
+        for(int i=0;i<n;i++)
+        {
+            // printing '*' in each column.
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
